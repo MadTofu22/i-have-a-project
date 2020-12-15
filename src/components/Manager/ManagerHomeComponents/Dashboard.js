a7f09f39380b4ce4bf1763e55166cf5434e772de
MadTofu22 <stutler.tom@gmail.com>
2020-12-15 15:14:02 -0600

    recommiting base files due to pulling down wrong starter repo

---
+++ src/components/Manager/ManagerHomeComponents/Dashboard.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+class Dashboard extends Component {
+    
+    state = {
+
+    }
+
+    render () {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default withRouter(connect()(Dashboard));
---
+++ src/components/Manager/ManagerHomeView.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+class ManagerHomeView extends Component {
+    
+    state = {
+
+    }
+
+    render () {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default withRouter(connect()(ManagerHomeView));
