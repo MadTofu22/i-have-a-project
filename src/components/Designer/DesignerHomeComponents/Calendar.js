027010e880f4df01a88280068b1dad2f07e0e82d
Khaildoran <sgermscheid@gmail.com>
2020-12-16 13:02:35 -0600

    added calendar.io dependencies

--- src/components/Designer/DesignerHomeComponents/Calendar.js
+++ src/components/Designer/DesignerHomeComponents/Calendar.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom'
-import mapStoreToProps from '../../redux/mapStoreToProps';
+import mapStoreToProps from '../../../redux/mapStoreToProps';
 
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
