d143d19c3b1fe811de8bc753c1925f1dd4c88ebc
RachelSmith500 <rachelsmith9721@gmail.com>
2020-12-17 11:31:00 -0600

    Designer Nav added

--- src/components/Designer/DesignerHomeView.js
+++ src/components/Designer/DesignerHomeView.js
@@ -2,15 +2,68 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom'
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import NavButtonDesigner from '../../Designer/DesignerHomeComponents/NavButtonDesigner.js'
+import Calendar from '../../DesignerHomeComponents/Calendar.js'
+import MyProfile from '../../DesignerHomeComponents/MyProfile.js'
+import Projects from '../../DesignerHomeComponents/projects.js'
 
 class DesignerHomeView extends Component {
 	state = {
 
 	};
 
-	render() {
+	render () {
+        const pages = [
+            {
+                path: '/home', 
+                label: 'Home'
+            },
+            {
+                path: '/DesignerHomeView/MyProfile', 
+                label: 'My Profile'
+            },
+            {
+                path: '/DesignerHomeView/Calendar', 
+                label: 'Calendar'
+            },
+            {
+                path: '/DesignerHomeView/projects', 
+                label: 'Projects'
+            },
+           
+		];
+	
 		return (
 			<>
+				<div>Designer Home</div>
+					<Router>
+						<div>Designer Home</div>
+							<div className='designerNavBar'>
+								{pages.map((page, index) => {
+									return <NavButtonDesigner key={index} page={page} />
+								})}
+							</div>
+							<Route
+								exact
+								path={`/home`}
+								component={Home}
+							/>
+							<Route
+								exact
+								path={`/DesignerHomeView/MyProfile`}
+								component={MyProfile}
+							/>
+							<Route
+								exact
+								path={`/DesignerHomeView/Calendar`}
+								component={Calendar}
+							/>
+							<Route
+								exact
+								path={`/DesignerHomeView/projects`}
+								component={Projects}
+							/>
+					</Router>
 			</>
 		);
 	}
