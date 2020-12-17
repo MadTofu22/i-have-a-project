08b3744bd3ff490af904488227cd08705aa26b2c
MadTofu22 <stutler.tom@gmail.com>
2020-12-16 18:20:16 -0600

    added NavButton component and nav bar code to manager home view, as well as the button labels and paths

--- src/components/Manager/ManagerHomeView.js
+++ src/components/Manager/ManagerHomeView.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
-import {withRouter} from 'react-router-dom';
+import {HashRouter as Router, Route, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+
+import NavButton from './ManagerHomeComponents/NavButton.js'
 
 class ManagerHomeView extends Component {
     
@@ -10,9 +12,39 @@ class ManagerHomeView extends Component {
     }
 
     render () {
+        const pages = [
+            {
+                path: '/dashboard', 
+                label: 'Dashboard'
+            },
+            {
+                path: '/designers', 
+                label: 'My Designers'
+            },
+            {
+                path: '/calendar', 
+                label: 'Calendar'
+            },
+            {
+                path: '/requests', 
+                label: 'Contract Requests'
+            },
+            {
+                path: '/search',
+                label: 'Find Designers'
+            },
+        ];
+
         return (
-            <>
-            </>
+            <Router>
+                <div className='managerNavBar'>
+                    {pages.map((page, index) => {
+                        return <NavButton key={index} page={page} />
+                    })}
+                </div>
+                {/* Routes to each component */}
+                
+            </Router>
         );
     }
 }
