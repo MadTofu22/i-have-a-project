feb78c5fab3dfca6d73e2fedfc39465f3ae183b9
Khaildoran <sgermscheid@gmail.com>
2020-12-17 11:45:42 -0600

    added route for page, and displaying temp data

--- src/components/Designer/DesignerHomeComponents/MyProfile.js
+++ src/components/Designer/DesignerHomeComponents/MyProfile.js
@@ -1,37 +1,43 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom'
-import mapStoreToProps from '../../redux/mapStoreToProps';
+import mapStoreToProps from '../../../redux/mapStoreToProps';
 
 class MyProfile extends Component {
 	state = {
 		profile: {
 			photo: '',
-			first_name: '',
-			last_name: '',
-			email: '',
-			phone: '',
-			rate: '',
-			linkedin: '',
+			first_name: 'Simon',
+			last_name: 'Germscheid',
+			email: 'email@email.com',
+			phone: '999-999-9999',
+			rate: 0,
+			linkedin: 'linkedin.site/profilename',
 			career: [{
-				start_date: '',
-				end_date: '',
-				title: ''
+				start_date: '05-20-18',
+				end_date: '05-20-20',
+				title: 'Legit Title'
 			}],
 			education: [{
-				graduation_date: '',
-				degree: '',
+				graduation_date: '01-15-21',
+				degree: 'Fullstack Software Engineering',
 			}],
 			certification: [{
 				graduation_date: '',
 				certification: ''
 			}],
-			skills: [{
-				label: '',
-				proficiency: ''
-			}],
+			skills: [
+				{
+					label: 'React',
+					proficiency: 5
+				},
+				{
+					label: 'JavaScript',
+					proficiency: 5
+				}
+			],
 			availability: [{
-				
+
 			}]
 		}
 	};
@@ -42,7 +48,7 @@ class MyProfile extends Component {
 			<div className="myProfileWrap">
 				<div className="left-ProfileColumn">
 					<div>
-						Hello, {this.state.profile.first_name, ' ',  this.state.profile.last_name}
+						Hello, {this.state.profile.first_name} {this.state.profile.last_name}
 					</div>
 					<div className="profileImg">
 
@@ -56,24 +62,29 @@ class MyProfile extends Component {
 					<div>
 						Career History: 
 						{this.state.profile.career.map( career => {
-							<p>{JSON.stringify(career)}</p>
+							return <p>{JSON.stringify(career)}</p>
 						})}
 					</div>
 					<div>
 						Education: 
 						{this.state.profile.education.map( school => {
-							<p>{JSON.stringify(school)}</p>
+							return <p>{JSON.stringify(school)}</p>
 						})}
 					</div>
 					<div>
 						Certifications: 
 						{this.state.profile.certification.map( cert => {
-							<p>{JSON.stringify(cert)}</p>
+							return <p>{JSON.stringify(cert)}</p>
 						})}
 					</div>
 				</div>
 				<div className="rightProfileColumn">
-
+				<div>
+						Skills: 
+						{this.state.profile.skills.map( skill => {
+							return <p>{JSON.stringify(skill)}</p>
+						})}
+				</div>
 				</div>
 			</div>
 		
