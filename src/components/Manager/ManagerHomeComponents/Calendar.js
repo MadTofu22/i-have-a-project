771294ad07ecabd5c5ace0638992b951c790a040
MadTofu22 <stutler.tom@gmail.com>
2020-12-18 10:53:45 -0600

    added more designer calendar blocks for test display

--- src/components/Manager/ManagerHomeComponents/Calendar.js
+++ src/components/Manager/ManagerHomeComponents/Calendar.js
@@ -9,7 +9,7 @@ import '../Manager.css';
 class ManagerCalendar extends Component {
     
     state = {
-        
+
     }
 
     render () {
@@ -25,6 +25,44 @@ class ManagerCalendar extends Component {
                     />
                 </div>
                 <h2 className='manager'>Designer Weekly Calendars</h2>
+                <div className='designerCalendarSection'>
+                    <div className='designerInfoBlock'>
+                        <h2 className='designerNameHeader'>Designer Name</h2>
+                        <button className='viewDesignerButton'>View Profile</button>
+                        <ul className='designerStatsLists'>
+                            <li>#Projects: 3</li>
+                            <li>Hours Available: 35</li>
+                            <li>Hours Scheduled: 0</li>
+                            <li>Billable Rate: $55</li>
+                        </ul>
+                    </div>
+                    <FullCalendar
+                        className='designerWeek'
+                        plugins={[dayGridPlugin]}
+                        initialView='dayGridWeek'
+                        height='20vh'
+                        dayMinWidth='5vw'
+                    />
+                </div>
+                <div className='designerCalendarSection'>
+                    <div className='designerInfoBlock'>
+                        <h2 className='designerNameHeader'>Designer Name</h2>
+                        <button className='viewDesignerButton'>View Profile</button>
+                        <ul className='designerStatsLists'>
+                            <li>#Projects: 3</li>
+                            <li>Hours Available: 35</li>
+                            <li>Hours Scheduled: 0</li>
+                            <li>Billable Rate: $55</li>
+                        </ul>
+                    </div>
+                    <FullCalendar
+                        className='designerWeek'
+                        plugins={[dayGridPlugin]}
+                        initialView='dayGridWeek'
+                        height='20vh'
+                        dayMinWidth='5vw'
+                    />
+                </div>
                 <div className='designerCalendarSection'>
                     <div className='designerInfoBlock'>
                         <h2 className='designerNameHeader'>Designer Name</h2>
