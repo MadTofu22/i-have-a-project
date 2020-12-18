99eb048804f1f76a6b4ca1f8c2ad0f5bc4e7ebc0
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-12-18 14:43:08 -0600

    added saga and reducer framework draft, no hard coding in yet

---
+++ src/redux/sagas/calendar.saga.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* sampleName() {
+  try {
+    const response = yield axios.get('/api/sampleName');
+    yield put({ type: 'SAMPLE_DISPATCH_CALL', payload: response.data });
+  } catch (error) {
+    console.log('User get request failed', error);
+  }
+}
+
+function* calendarSaga() {
+  yield takeLatest('FETCH_USER', sampleName);
+}
+
+export default calendarSaga;
