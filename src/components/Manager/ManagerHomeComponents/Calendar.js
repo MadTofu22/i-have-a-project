122d114d00bacec33cd459c19829fba5e6057c0b
MadTofu22 <stutler.tom@gmail.com>
2020-12-17 19:24:29 -0600

    fullcalendar imports in, manager month calendar displaying

--- src/components/Manager/ManagerHomeComponents/Calendar.js
+++ src/components/Manager/ManagerHomeComponents/Calendar.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import mapStoreToProps from '../../../redux/mapStoreToProps';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import '../Manager.css';
 
 class ManagerCalendar extends Component {
     
@@ -12,6 +15,13 @@ class ManagerCalendar extends Component {
     render () {
         return (
             <>
+                <div className='managerCalendarSection'>
+                    <h3>My Calendar</h3>
+                    <FullCalendar
+                        plugins={[dayGridPlugin]}
+                        initialView='dayGridMonth'
+                    />
+                </div>
             </>
         );
     }
