a80ba5b2dd190696c0e2a4cdf0af97dc33941610
MadTofu22 <stutler.tom@gmail.com>
2020-12-27 18:32:40 -0600

    added skill removal working, rating working

--- src/components/Designer/UpdateProfileComponents/AddSkills.js
+++ src/components/Designer/UpdateProfileComponents/AddSkills.js
@@ -3,16 +3,26 @@ import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom'
 import mapStoreToProps from '../../../redux/mapStoreToProps';
 
+import AddedSkillLabel from './AddedSkillLabel';
+
 class AddSkills extends Component {
+	
 	state = {
 		softwareList: ['Software 1', 'Software 2', 'Software 3', 'Software 4', 'Software 5'],
 		selectedSoftware: [],
+		skillsList: [
+			{
+				label: 'Communication',
+				rating: '1',
+			},
+		],
 		saveDisabled: false,
-
 	};
 
+	// This fucntion saves the input info and stores it to the redux state, then passes it to the server/database
 	handleFormSubmit = (event) => {
-		
+		console.log('skill form save clicked:', event.target);
+
 	}
 
 	// This function toggles the Save button enabled or disabled
@@ -26,7 +36,29 @@ class AddSkills extends Component {
 		}
 	}
 
+	// This function updates the local state skills list with User added skills and their ratings.
+	updateSkill = (name, rating) => {
+
+	}
+
+	// This function removes a skill from the local state skills list by it's index
+	removeSkill = (index) => {
+		let newSkillsList = [];
+		for (let skillIndex in this.state.skillsList) {
+			if (index !== Number(skillIndex)) {
+				newSkillsList.push(this.state.skillsList[skillIndex])
+			}
+			console.log('removed skillIndex:', Number(skillIndex))
+		}
+		console.log('in removeSkill, index:', index, '; newSkillsList:', newSkillsList)
+		this.setState({
+			...this.state,
+			skillsList: newSkillsList,
+		});
+	}
+
 	render() {
+		console.log('AddSkills:', this)
 		return (
 			<>
 				<h2>Add Skills</h2>
@@ -46,7 +78,9 @@ class AddSkills extends Component {
 							<label htmlFor='skillInput'>Enter Skill</label>
 							<input type='text' name='skillInput' id='skillInput' className='skillsTextField' />
 							<input type='button' id='addSkill' value='Add' />
-							
+							{this.state.skillsList.map((skill, index) => {
+								return <AddedSkillLabel key={index} index={index} skill={skill} removeSkill={this.removeSkill} updateSkill={this.updateSkill} />
+							})}
 						</div>
 						<br/>
 						<input type='submit' disabled={this.state.saveDisabled} value='Save' />
--- src/components/Designer/UpdateProfileComponents/AddedSkillLabel.js
+++ src/components/Designer/UpdateProfileComponents/AddedSkillLabel.js
@@ -5,11 +5,47 @@ import mapStoreToProps from '../../../redux/mapStoreToProps';
 
 class AddedSkillLabel extends Component {
 
+    state = {
+        label: this.props.skill.label,
+        rating: this.props.skill.rating,
+        newRating: '',
+    }
+
+    // This function handles updating the local state with the changed rating number
+    handleRatingChange = event => {
+        console.log('newRating:', event.target.value);
+        this.setState({
+            ...this.state,
+            newRating: event.target.value
+        });
+    }
+
     render () {
+        console.log(this)
         return (
-            <>
-                <h4></h4>
-            </>
+            <div className='skillLabelContainer'>
+                <h4>{this.state.label}</h4>
+                <input 
+                    type='number' 
+                    className='ratingInput' 
+                    defaultValue={this.props.skill.rating} 
+                    min='1' 
+                    max='5' 
+                    onChange={this.handleRatingChange} 
+                />
+                <input 
+                    type='button' 
+                    className='skillButton' 
+                    value='Save Rating' 
+                    onClick={() => this.props.updateSkill(this.state.label, this.state.newRating)} 
+                />
+                <input 
+                    type='button' 
+                    className='skillButton' 
+                    value='Delete' 
+                    onClick={() => this.props.removeSkill(this.props.index)} 
+                />
+            </div>
         );
     }
 }
