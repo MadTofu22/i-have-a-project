b48f6d5184fe1bb5d2e6fa5dd988bc501468bcdc
Rachel Smith <rachelsmith@Rachels-MacBook-Pro.local>
2021-01-04 15:36:59 -0600

    outbox saga added

--- src/redux/sagas/outbox.saga.js
+++ src/redux/sagas/outbox.saga.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
+function* fetchOutbox (){
+    try{
+        console.log('outbox');
+        
+        const response = yield axios.get('/api/managers');
+        console.log(response.data);
+
+        yield put ({ type: 'SET_OUTBOX', payload: response.data});
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
-
-
-
-
+function* outboxSaga() {
+	yield takeLatest('FETCH_OUTBOX', fetchOutbox);
+}
 
 export default outboxSaga;
