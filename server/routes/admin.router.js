5e265e9d82d7f3830f5164f0d8fd98df409d8ccd
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2021-01-05 16:34:55 -0600

    developed order of operations for delete statements, implementing SQL transactions and statements

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -19,11 +19,54 @@ router.delete('/', async (req, res) => {
         await connection.query('BEGIN');
         console.log("the user is",req.user.id);
 
-        // Check if the user is authenticated
-        let projListSQL = '';
+        // IF IT'S A MANAGER, DO THE FOLLOWING STEPS.
+        // IF IT'S A DESIGNER, GET THE ASSOCIATED DESIGNER_ID AND SKIP TO STEP 3.
 
-        
-        managerDelSQL = `SELECT * FROM "" WHERE "user_id" = $1`;
+
+        // 1. Use id from table user to delete from contract_requests (?) using id in requesting_manager_id and contracted_manager_id, 
+        const sqlOne = `DELETE FROM "contract_requests" WHERE requesting_manager_id = $1 OR contracted_manager_id = $1` ;
+        await connection.query(sqlOne, [req.body.id]);
+
+        // 2. Use user id to get designer list
+        const sqlTwo = `SELECT 'id' FROM "designers" WHERE manager_id = $1`;
+        let designerIdArray = await connection.query( sqlTwo, [req.body.id]);
+
+        // 1a. Use id to get designer_id
+        // const sqlAltOne = `SELECT designer_id FROM "users" WHERE 'id' = $1`;
+        // let designerIdArray = [];
+        // let altReference = await connection.query( sqlAltOne, [req.body.id]);
+        // designerIdArray.push(altReference);
+
+        // 3. Use designer id list to eliminate values from the following table using designer_id:
+        //     1. Users
+        const sqlDesignerOne = `` ;
+        let  = await connection.query( sqlOne, [element]);
+        //     2. Skills
+        const sqlDesignerTwo = `` ;
+        //     3. projects_designers_join
+        const sqlDesignerThree = `` ;
+        //     4. designer_calendar_item
+        const sqlDesignerFour = `` ;
+        //     5. Career
+        const sqlDesignerFive = `` ;
+        //     6. Education
+        const sqlDesignerSix = `` ;
+        //     7. designer_software_join
+        const sqlDesignerSeven = `` ;
+        //     8. contract_requests (?)
+        const sqlDesignerEight = `` ;
+        // 4. Delete from “designers”
+        const sqlThree = `` ;
+        // 5. Use manager_id from “projects” to get id, use to delete from projects_designer_join.
+        const sqlFour = `` ;
+        // 6. Delete from “projects”
+        const sqlFive = `` ;
+        // 7. Delete from Users using manager_id
+        const sqlSix = `` ;
+
+        // Check if the user is authenticated
+        let projListSQL = ``;
+
         
 
         let rawFiles = await connection.query( managerDelSQL, [1]);
