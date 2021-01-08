54d181df7686224d23ddd1488fb121c7cc2de825
MadTofu22 <stutler.tom@gmail.com>
2021-01-07 21:34:52 -0600

    converted update profile to single page due to lifecycle timing issues

--- src/components/Designer/UpdateProfileComponents/AddInformation.js
+++ src/components/Designer/UpdateProfileComponents/AddInformation.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom'
 import mapStoreToProps from '../../../redux/mapStoreToProps';
 import TextField from '@material-ui/core/TextField';
+import AddedSkillLabel from './AddedSkillLabel';
 
 class AddInformation extends Component {
 
@@ -22,13 +23,88 @@ class AddInformation extends Component {
 		};
 	}
 
-	componentDidMount = () => {
-		this.props.dispatch({
-			type: 'FETCH_PROFILE',
-			payload: this.props.store.user.designer_id
+	// This function marks the state for the selected software as checked and toggles the save button enabled
+	handleCheckboxChange = (index) => {
+
+		let newSoftwareList = this.state.profile.software.slice();
+		newSoftwareList[index].proficient = !this.state.profile.software[index].proficient;
+
+		console.log('in handleCheckBoxChange - index:', index, '; newSoftwareList:', newSoftwareList);
+
+		this.setState({
+			...this.state,
+			profile: {
+				...this.state.profile,
+				software: newSoftwareList,
+			}
 		});
 	}
 
+	// This function updates the local state with the current input in the Enter Skill field
+	handleSkillInputChange = (event) => {
+
+		console.log(this.skillInputRef)
+		this.setState({
+			...this.state,
+			currentSkillInput: this.skillInputRef.current.value,
+		});
+	}
+
+	// This function adds a skill to the skills list array and sets the default rating to 3
+	addSkill = () => {
+
+		this.setState({
+			...this.state,
+			profile: {
+				...this.state.profile,
+				skills: [
+					...this.state.profile.skills,
+					{
+						label: this.skillInputRef.current.value,
+						proficiency: '3',
+					}
+				]
+			}
+		});
+		this.skillInputRef.current.value = '';	
+	}
+
+	// This function updates the local state skills list with User added skills and their ratings.
+	updateSkill = (index, newRating) => {
+
+		console.log('in updateSkill - index:', index, '; newRating:', newRating)
+
+		let newSkillsList = this.state.profile.skills.slice();
+		newSkillsList[index] = {label: this.state.profile.skills[index].label, proficiency: newRating};
+
+		console.log('in updateSkill - newSkillList:', newSkillsList, 'state skillsList:', this.state.skillsList);
+
+		this.setState({
+			...this.state,
+			profile: {
+				...this.state.profile,
+				skills: newSkillsList,
+			},
+		});
+	}
+
+	// This function removes a skill from the local state skills list by it's index
+	removeSkill = (index) => {
+
+		let newSkillsList = this.state.profile.skills.slice();
+		newSkillsList.splice(index, 1);
+
+		console.log('in removeSkill - newSkillList:', newSkillsList)
+
+		this.setState({
+			...this.state,
+			profile: {
+				...this.state.profile,
+				skills: newSkillsList,
+			},
+		});
+	}
+	
 	// This function handles storing the work and education history inputs in the local state on change
 	handleInputChange = (event, section, property) => {
 
@@ -84,144 +160,190 @@ class AddInformation extends Component {
 	render() {
 		console.log('UpdateProfile/info has rendered - this.props:', this.props, this.state)
 		return (
-			<div className="addInfoWrap">
-				<h2>Add Information</h2>
-				<br/>
-				<label
-					htmlFor='phoneNum'
-					className='buildProfileLabel'
-				>Phone Number:
-				</label>
-				<TextField
-					type='text'
-					id='phoneNum'
-					defaultValue={this.state.profile.designer ? this.state.profile.designer.phone : ''}
-					onChange={(event) => this.handleInputChange(event, 'designer', 'phone')}
-				/>
-				<label
-					htmlFor='imgUrl'
-					className='buildProfileLabel'
-				>Profile Image URL:
-				</label>
-				<TextField 
-					type='text'
-					id='imgUrl'
-					defaultValue={this.state.profile.designer ? this.state.profile.designer.photo : ''}
-					onChange={(event) => this.handleInputChange(event, 'designer', 'photo')}
-				/>
-				<br/>
-				<label
-					htmlFor='linkedinUrl'
-					className='buildProfileLabel'
-					>LinkedIn Page:
-				</label>
-				<TextField 
-					type='text'
-					id='linkedinUrl'
-					onChange={(event) => this.handleInputChange(event, 'designer', 'linkedin')}
-					defaultValue={this.state.profile.designer ? this.state.profile.designer.linkedin : ''}
-				/>
-				<br/>
+			<>
+				<div className="addInfoWrap">
+					<h2>Add Information</h2>
+					<br/>
+					<label
+						htmlFor='phoneNum'
+						className='buildProfileLabel'
+					>Phone Number:
+					</label>
+					<TextField
+						type='text'
+						id='phoneNum'
+						defaultValue={this.state.profile.designer ? this.state.profile.designer.phone : ''}
+						onChange={(event) => this.handleInputChange(event, 'designer', 'phone')}
+					/>
+					<label
+						htmlFor='imgUrl'
+						className='buildProfileLabel'
+					>Profile Image URL:
+					</label>
+					<TextField 
+						type='text'
+						id='imgUrl'
+						defaultValue={this.state.profile.designer ? this.state.profile.designer.photo : ''}
+						onChange={(event) => this.handleInputChange(event, 'designer', 'photo')}
+					/>
+					<br/>
+					<label
+						htmlFor='linkedinUrl'
+						className='buildProfileLabel'
+						>LinkedIn Page:
+					</label>
+					<TextField 
+						type='text'
+						id='linkedinUrl'
+						onChange={(event) => this.handleInputChange(event, 'designer', 'linkedin')}
+						defaultValue={this.state.profile.designer ? this.state.profile.designer.linkedin : ''}
+					/>
+					<br/>
 
-				{/* This code was used when we intended to handle availability as subtractive */}
-				{/* <label
-					htmlFor='availability_hours'
-					className='buildProfileLabel'
-					>Hours Available per Week:
-				</label>
-				<TextField 
-					type='text'
-					id='availability_hours'
-					onChange={(event) => this.handleInputChange(event, 'designer', 'availability_hours')}
-					defaultValue={this.state.profile.designer ? this.state.profile.designer.availability_hours : ''}
-				/>
-				<br/>
-				
-				<label
-					htmlFor='availability_hours'
-					className='buildProfileLabel'
-					>Available on Weekends?
-				</label>
-				<input 
-					type='checkbox'
-					id='availability_hours'
-					onChange={(event) => this.handleInputChange(event, 'designer', 'availability_hours')}
-					defaultValue={this.state.profile.designer.availability_hours}
-				/>
-				<br/> */}
-				<label
-					htmlFor='educationDegree'
-					className='buildProfileLabel'
-				>Degree:
-				</label>
-				<TextField 
-					type='text'
-					id='educationDegree'
-					ref={this.educationTitleInputRef}
-					onChange={(event) => this.handleHistoryInputChange(event, 'education', 'degree')}
-				/>
-				<label
-					htmlFor='educationDegree'
-					className='buildProfileLabel'
-				>Institution:
-				</label>
-				<TextField 
-					type='text'
-					id='educationLocation'
-					ref={this.educationLocationInputRef}
-					onChange={(event) => this.handleHistoryInputChange(event, 'education', 'location')}
-				/>
-				<button onClick={() => this.updateHistoryList('education')}>Add</button>
-				<h4>Added Education</h4>
-				<ul>
-					{this.state.profile.education ? this.state.profile.education.map((row, index) => {
-						return <li key={index}>{row.degree} from {row.location}</li>
+					{/* This code was used when we intended to handle availability as subtractive */}
+					{/* <label
+						htmlFor='availability_hours'
+						className='buildProfileLabel'
+						>Hours Available per Week:
+					</label>
+					<TextField 
+						type='text'
+						id='availability_hours'
+						onChange={(event) => this.handleInputChange(event, 'designer', 'availability_hours')}
+						defaultValue={this.state.profile.designer ? this.state.profile.designer.availability_hours : ''}
+					/>
+					<br/>
+					
+					<label
+						htmlFor='availability_hours'
+						className='buildProfileLabel'
+						>Available on Weekends?
+					</label>
+					<input 
+						type='checkbox'
+						id='availability_hours'
+						onChange={(event) => this.handleInputChange(event, 'designer', 'availability_hours')}
+						defaultValue={this.state.profile.designer.availability_hours}
+					/>
+					<br/> */}
+					<label
+						htmlFor='educationDegree'
+						className='buildProfileLabel'
+					>Degree:
+					</label>
+					<TextField 
+						type='text'
+						id='educationDegree'
+						ref={this.educationTitleInputRef}
+						onChange={(event) => this.handleHistoryInputChange(event, 'education', 'degree')}
+					/>
+					<label
+						htmlFor='educationDegree'
+						className='buildProfileLabel'
+					>Institution:
+					</label>
+					<TextField 
+						type='text'
+						id='educationLocation'
+						ref={this.educationLocationInputRef}
+						onChange={(event) => this.handleHistoryInputChange(event, 'education', 'location')}
+					/>
+					<button onClick={() => this.updateHistoryList('education')}>Add</button>
+					<h4>Added Education</h4>
+					<ul>
+						{this.state.profile.education ? this.state.profile.education.map((row, index) => {
+							return <li key={index}>{row.degree} from {row.location}</li>
+						}) : ''}
+					</ul>
+					<br/>
+					<label
+						htmlFor='careerTitle'
+						className='buildProfileLabel'
+					>
+						Position:
+					</label>
+					<TextField 
+						type='text'
+						id='careerTitle'
+						ref={this.careerTitleInputRef}
+						onChange={(event) => this.handleHistoryInputChange(event, 'career', 'title')}
+					/>
+					<label
+						htmlFor='careerTitle'
+						className='buildProfileLabel'
+					>
+						Company:
+					</label>
+					<TextField 
+						type='text'
+						id='careerLocation'
+						ref={this.careerLocationInputRef}
+						onChange={(event) => this.handleHistoryInputChange(event, 'career', 'location')}
+					/>
+					<button onClick={() => this.updateHistoryList('career')}>Add</button>
+					<h4>Added Work Experience</h4>
+					<ul>
+						{this.state.profile.career ? this.state.profile.career.map((job, index) => {
+							return <li key={index}>{job.title} at {job.location}</li>
+						}) : ''}
+					</ul>
+					<br/>
+					<button
+						onClick={() => {this.props.saveAndNavigate('/DesignerHomeView', this.state.profile)}}
+					>
+						Save and Go Home
+					</button>
+					<button
+						onClick={() => {this.props.saveAndNavigate('/UpdateProfile/Skills', this.state.profile)}}
+					>
+						Save and go to Skills
+					</button>
+				</div>
+				<div className="addInfoWrap">
+					<h2>Add Skills</h2>
+					<h4>Software</h4>
+					{this.state.profile.software ? this.state.profile.software.map((software, index) => {
+						return (
+							<>
+								<input 
+									type='checkbox'
+									id={`software${index}`} 
+									name={`software${index}`} 
+									key={index} 
+									value={software.label}
+									checked={software.proficient}
+									onChange={() => this.handleCheckboxChange(index)} 
+								/>
+								<label htmlFor={`software${index}`}>{software.label}</label>
+								<br/>
+							</>
+						)
 					}) : ''}
-				</ul>
-				<br/>
-				<label
-					htmlFor='careerTitle'
-					className='buildProfileLabel'
-				>
-					Position:
-				</label>
-				<TextField 
-					type='text'
-					id='careerTitle'
-					ref={this.careerTitleInputRef}
-					onChange={(event) => this.handleHistoryInputChange(event, 'career', 'title')}
-				/>
-				<label
-					htmlFor='careerTitle'
-					className='buildProfileLabel'
-				>
-					Company:
-				</label>
-				<TextField 
-					type='text'
-					id='careerLocation'
-					ref={this.careerLocationInputRef}
-					onChange={(event) => this.handleHistoryInputChange(event, 'career', 'location')}
-				/>
-				<button onClick={() => this.updateHistoryList('career')}>Add</button>
-				<h4>Added Work Experience</h4>
-				<ul>
-					{this.state.profile.career ? this.state.profile.career.map((job, index) => {
-						return <li key={index}>{job.title} at {job.location}</li>
-					}) : ''}
-				</ul>
-				<br/>
-				<button
-					onClick={() => {this.props.saveAndNavigate('/DesignerHomeView', this.state.profile)}}
-				>
-					Save and Go Home
-				</button>
-				<button
-					onClick={() => {this.props.saveAndNavigate('/UpdateProfile/Skills', this.state.profile)}}
-				>
-					Save and go to Skills
-				</button>
-			</div>
+					<div className='skillsSection'>
+						<label htmlFor='skillInput'>Enter Skill</label>
+						<input type='text' name='skillInput' id='skillInput' className='skillsTextField' ref={this.skillInputRef} onChange={this.handleSkillInputChange} />
+						<input type='button' id='addSkill' value='Add' onClick={this.addSkill} />
+						{this.state.profile.skills ? this.state.profile.skills.map((skill, index) => {
+							return <AddedSkillLabel 
+										key={index} 
+										index={index} 
+										skill={skill} 
+										removeSkill={this.removeSkill} 
+										updateSkill={this.updateSkill} 
+									/>
+						}) : ''}
+					</div>
+					<br/>
+					{/* <button
+						onClick={() => {this.props.saveAndNavigate('/UpdateProfile/Info', this.state.profile)}}
+					>Save and go to Information
+					</button> */}
+					<button
+						onClick={() => {this.props.saveAndNavigate('/DesignerHomeView', this.state.profile)}}
+					>Save and Go Home
+					</button>
+				</div>
+			</>
 		);
 	}
 }
