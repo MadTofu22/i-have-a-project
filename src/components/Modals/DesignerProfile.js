b50876790bb20bb70f127dae9d312c8a52e9a459
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2021-01-09 10:03:59 -0600

    added shell for designerProfile modal

---
+++ src/components/Modals/DesignerProfile.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {connect} from 'react-redux'
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+import MenuItem from '@material-ui/core/MenuItem';
+
+
+
+function DesignerProfile(props) {
+    const [open, setOpen] = React.useState(false);
+  
+  
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+    const handleAddDesigner = (designerObj) => {
+      props.dispatch({
+        type: "ADD_DESIGNER_TO_PROJECT",
+        payload: {...designerObj, project_id: props.project_id}
+      })
+      setOpen(false);
+    }
+    
+  
+    return (
+      <div>
+  
+      <button onClick={handleClickOpen}>Add Team Member</button> 
+  
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+          <DialogTitle id="title">Select Team Members</DialogTitle>
+          {props.store.designer.length > 0 ? 
+              <DialogContent>
+                   {props.store.designer.map( (designer) => {
+                      return( 
+                          <MenuItem 
+                              onClick={() => handleAddDesigner(designer)} 
+                              key={designer.designer_id}
+                          > {designer.first_name + ' ' + designer.last_name}
+                          </MenuItem>
+                      )
+                  })}
+              </DialogContent>
+            :
+            <DialogContent>
+            <DialogContentText>
+            You don't have any designers yet!
+            </DialogContentText>
+          </DialogContent>
+            
+          }
+        </Dialog>
+      </div>
+    );
+  }
+  
+  export default connect(mapStoreToProps)(DesignerProfile);
