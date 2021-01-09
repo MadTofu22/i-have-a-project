b3ae722b7ad037013a9199fbac6630637942f017
Khaildoran <sgermscheid@gmail.com>
2021-01-09 15:35:33 -0600

    profile modal styling updates

--- src/components/Designer/DesignerHomeComponents/MyProfile.js
+++ src/components/Designer/DesignerHomeComponents/MyProfile.js
@@ -3,6 +3,10 @@ import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom'
 import mapStoreToProps from '../../../redux/mapStoreToProps';
 
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import Divider from '@material-ui/core/Divider'
+
 import '../Designer.css'
 
 class MyProfile extends Component {
@@ -15,9 +19,11 @@ class MyProfile extends Component {
 	render() {
 		return (
 			<div className="componentViewWrap">
+				<h3 className='pageTitle'>My Profile:</h3>
+
 				<div className="myProfileWrap">
 					<div className="left-ProfileColumn">
-						
+	
 						<div className="profileImg">
 							{this.props.store.profile.designer ?
 								<img
@@ -33,24 +39,31 @@ class MyProfile extends Component {
 						<div className="profileName">
 							{this.props.store.user.first_name} {this.props.store.user.last_name}
 						</div>
+						<br/>
+						<Divider className="menuDivider"  variant="middle"/>
 						<div className="profileContactInfo">
-							<h2>Contact Info: </h2>
-							<p>Email: {this.props.store.user ? this.props.store.user.email : ''}</p>
-							<p>Phone: {this.props.store.profile.designer ? this.props.store.profile.designer.phone : ''}</p>
-							<p>Linkedin URL: {this.props.store.profile.designer ? this.props.store.profile.designer.linkedin : ''}</p>
+							<div className="sectionHeader">Contact Info: </div>
+							<div className="sectionList">
+								<p>Email: {this.props.store.user ? this.props.store.user.email : ''}</p>
+								<p>Phone: {this.props.store.profile.designer ? this.props.store.profile.designer.phone : ''}</p>
+								<p>Linkedin URL: {this.props.store.profile.designer ? this.props.store.profile.designer.linkedin : ''}</p>
+							</div>
 						</div>
 						<div>
-							<h2>Career History: </h2>
-							{this.props.store.profile.career ?
-								this.props.store.profile.career.map((career, index) => {
-									return <p className='careerItem' key={index}>{career.title} at {career.location}</p>
-								})
-								:
-								'No Career History Added'
-							}
+							<div className='sectionHeader'>Career History: </div>
+								<div className="sectionList">
+									{this.props.store.profile.career ?
+									this.props.store.profile.career.map((career, index) => {
+										return <p className='careerItem' key={index}>{career.title} at {career.location}</p>
+									})
+									:
+									'No Career History Added'
+									}
+								</div>
 						</div>
 						<div>
-							<h2>Education:</h2>
+							<div className='sectionHeader'>Education:</div>
+								<div className="sectionList">
 							{this.props.store.profile.education ?
 								this.props.store.profile.education.map((degree, index) => {
 									return <p className='educationItem' key={index}>{degree.degree} at {degree.location}</p>
@@ -58,30 +71,49 @@ class MyProfile extends Component {
 								:
 								'No Education History Added'
 							}
+								</div>
 						</div>
 					</div>
 					<div className="right-ProfileColumn">
-							<div>
-								<h2>Software:</h2>
-								{this.props.store.profile.software ?
-									this.props.store.profile.software.map((software, index) => {
-										if (software.proficient) {
-											return <p className='softwareItem' key={index}>{software.label}</p>
+							<div >
+								<div  className='sectionHeader'>Software:</div>
+									<div className="sectionList">
+										{this.props.store.profile.software ?
+										this.props.store.profile.software.map((software, index) => {
+											if (software.proficient) {
+												return <p className='softwareItem' key={index}>{software.label}</p>
+											}
+										})
+										:
+										'No Proficient Software Listed'
 										}
-									})
-									:
-									'No Proficient Software Listed'
-								}
+									</div>						
 							</div>
 							<div>
-								<h2>Skills:</h2>
+								<div className='sectionHeader'>Skills:</div>
+										<br/>
+										<div className="sectionList" style={{overflow: 'scroll'}}>
 								{this.props.store.profile.skills ?
-									this.props.store.profile.skills.map((skill, index) => {
-										return <p className='skillItem' key={index}>{skill.label} - {skill.proficiency}/5</p>
+									this.props.store.profile.skills.map((skill) => {
+										return (<>
+												<Typography gutterBottom>
+													{skill.label}
+												</Typography>
+												<Slider 
+													style={{width: '90%'}}
+													value={skill.proficiency}
+													step={1}
+													marks
+													min={1}
+													max={5}  
+												/>
+												</>)
+					
 									})
 									:
 									'No Additional Skills Added'
 								}
+								 </div>
 							</div>
 					</div>
 				</div>
